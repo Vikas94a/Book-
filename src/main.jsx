d984481437d76413276e5ccc99ddfa0e1c88b147
Vikas94a <vikas1.kodehode@gmail.com>
2025-02-07 09:01:59 +0100

    update SearchBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Route,Link, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {  RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import ErrorView from './view/ErrorView.jsx'
 import HomeView from './view/Homeview.jsx'
 import BookByCategory from './view/BookByCategory.jsx'
 import BookView from './view/BookView.jsx'
 import FavBookView from './view/FavBookView.jsx'
+
 
 const router = createBrowserRouter([{
   path: "/Book-",
